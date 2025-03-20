8d3452bfc253b7640c7ad285e971db5d4214f65b
Chaganti Pavan Chandra Vishal (MS/ECE-XC-FLVV) <bgp3kor@bosch.com>
2025-03-20 19:59:54 +0530

    add_files

---
+++ Assignment-2/Task2.py
@@ -0,0 +1,9 @@
+# Initializing sum variable
+total_sum = 0
+
+# Using a for loop to sum numbers from 1 to 50
+for num in range(1, 51):
+    total_sum += num
+
+# Displaying the final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
